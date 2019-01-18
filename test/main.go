ae66d572073e921e9843fc2581d92fd9144a070c
liwansheng <lws99927800@163.com>
2019-01-18 12:23:56 +0800

    test

--- test/main.go
+++ test/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
 	"fmt"
 	"github.com/NBSChain/go-gossip-tcp"
+	"github.com/NBSChain/go-nbs/utils"
 	"github.com/spf13/cobra"
+	"io/ioutil"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -51,4 +58,66 @@ func mainRun(cmd *cobra.Command, args []string) {
 	}
 
 	node.Run()
+}
+
+type msgManager struct {
+	nodeId string
+	uKey   string
+}
+
+const (
+	configFile    = "gsp.dat"
+	dataSeparator = "++++++"
+)
+
+func NewMsgManager() msgManager {
+
+	manager := &msgManager{}
+
+	if _, ok := utils.FileExists(configFile); !ok {
+		manager.createNodeId()
+	} else {
+		manager.readNodeId()
+	}
+
+	return *manager
+}
+
+func (msg *msgManager) createNodeId() {
+
+	file, err := os.Create(configFile)
+	if err != nil {
+		panic(err)
+	}
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 256)
+	if err != nil {
+		panic(err)
+	}
+
+	publicKey := &privateKey.PublicKey
+	pubData := x509.MarshalPKCS1PublicKey(publicKey)
+	nodeId := base64.StdEncoding.EncodeToString(pubData)
+
+	priData := x509.MarshalPKCS1PrivateKey(privateKey)
+	uKey := base64.StdEncoding.EncodeToString(priData)
+
+	data := strings.Join([]string{nodeId, uKey}, dataSeparator)
+
+	if _, err := file.Write([]byte(data)); err != nil {
+		panic(err)
+	}
+
+	msg.nodeId = nodeId
+	msg.uKey = uKey
+}
+
+func (msg *msgManager) readNodeId() {
+	b, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		panic(err)
+	}
+	data := strings.Split(string(b), dataSeparator)
+	msg.nodeId = data[0]
+	msg.uKey = data[1]
 }
